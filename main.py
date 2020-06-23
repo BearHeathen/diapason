08ceca718264db215a6cfd70f4be49cd7a15b4f7
Brandon Stewart <b.stewart@mjolnirsoftware.com>
2020-06-23 15:19:58 -0500

    Continuing to build
    Added some resources, added a play functionality.

--- main.py
+++ main.py
@@ -3,29 +3,40 @@
 # DIAPASON - A grand swelling burst of harmony; just octave in Pythagorean tuning
 
 from tkinter import *
+from pygame import mixer
 
 # FUNCTIONS
 # ===========
 
-def play_btn():
-    print('Hey, this button works!')
+def play_music():
+    mixer.music.load("assets/chant.mp3")
+    mixer.music.play()
+    print('Make a joyful noise!')
 
 # MAIN
 #============
 root = Tk()
 
+
+# Initialize Pygame Mixer class for audio playback
+mixer.init()
+
 root.geometry('600x400')
 root.title("Diapason")
 root.iconbitmap(r'assets/diapason.ico')
-
-text = Label(root, text = 'Let\'s make some noise!')
-text.pack()
 
 playImg = PhotoImage(file='assets/play.png')
 lblPlay = Label(root, text="Play")
 lblPlay.pack()
 
-btnPlay = Button(root, image = playImg, command=play_btn)
+# Main Screen background.
+bckgImg = PhotoImage(file='assets/monk1.png') 
+text = Label(root, text = 'Make a joyful noise!', image = bckgImg)
+text.pack()
+
+
+
+btnPlay = Button(root, image = playImg, command=play_music)
 btnPlay.pack()
 
 root.mainloop()
