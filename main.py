5055905f75d17d7b1a2033ed35b8ce6dc3ffaebf
Brandon Stewart <b.stewart@mjolnirsoftware.com>
2020-06-26 12:17:10 -0500

    Added playlist functionality.

--- main.py
+++ main.py
@@ -26,6 +26,18 @@ from mutagen.mp3 import MP3
 # FUNCTIONS
 # ===========
 
+# Utility functions
+def strip_filename(filename):
+    tmp = os.path.basename(filename)
+    nowplaying = os.path.splitext(tmp)[0]
+    return nowplaying
+
+def resize_btn_image(path_to_img, x_dim, y_dim):
+    original_image = Image.open(path_to_img)
+    original_image = original_image.resize((x_dim, y_dim), Image.ANTIALIAS)
+    new_image = ImageTk.PhotoImage(original_image)
+    return new_image
+
 def about_us():
     tkinter.messagebox.showinfo('About Diapasaon', '''        A joyfully polyphonous yet reverent music player
         Built using Python and Tkinter
@@ -34,6 +46,16 @@ def about_us():
 def browse_file():
     global filename
     filename = filedialog.askopenfilename()
+    add_to_playlist(filename)
+global play_selection
+playlist = []
+def add_to_playlist(playlist_filename):
+    playlist_filename = strip_filename(playlist_filename)
+    index = 0
+    lboxPlaylist.insert(index, playlist_filename)
+    playlist.insert(index, filename) # gets the whole file+path
+    lboxPlaylist.pack()
+    index += 1
 
 def show_details(name_of_file):
 
@@ -58,32 +80,52 @@ def show_details(name_of_file):
     t1 = threading.Thread(target=start_count, args=(total_length,))
     t1.start()
 
-
 def start_count(length):
-    while(length):
-        mins, secs = divmod(length, 60)
-        mins = round(mins)
-        secs = round(secs)
-        time_format = '{:02d}:{:02d}'.format(mins, secs)
-        lblCurrent['text'] = time_format
-        time.sleep(1)
-        length -= 1
+    current_time=0
+    while(current_time <= length and mixer.music.get_busy()):
+        if is_paused:
+            continue
+        else:
+            mins, secs = divmod(current_time, 60)
+            mins = round(mins)
+            secs = round(secs)
+            time_format = '{:02d}:{:02d}'.format(mins, secs)
+            lblCurrent['text'] = time_format
+            time.sleep(1)
+            current_time += 1
 
+def remove_item():
+    selected_song = lboxPlaylist.curselection()
+    selected_song = int(selected_song[0])
+    lboxPlaylist.delete(selected_song)
+    playlist.pop(selected_song)
+    print(playlist)
+
+# Player Functions
 is_paused = False
 def play_music():
     global is_paused
     if is_paused:
         mixer.music.unpause()
-        nowplaying = strip_filename(filename)
+        selected_song = lboxPlaylist.curselection()
+        selected_song = int(selected_song[0])
+        play_selection = playlist[selected_song]
+        nowplaying = strip_filename(play_selection)
         statusbar['text'] = "Resumed Playing" + ' ' + nowplaying
         is_paused = False
     else:
         try:
-            mixer.music.load(filename)
+            stop_music()
+            time.sleep(1)
+            selected_song = lboxPlaylist.curselection()
+            selected_song = int(selected_song[0])
+            
+            play_selection = playlist[selected_song]
+            mixer.music.load(play_selection)
             mixer.music.play()
-            show_details(filename)
+            show_details(play_selection)
             # Show name of playing media in statusbar
-            nowplaying = strip_filename(filename)
+            nowplaying = strip_filename(play_selection)
             statusbar['text'] = "Playing" + ' ' + nowplaying
         except:
             tkinter.messagebox.showerror('Error Playing', 'Diapason experienced an error. Please try again.')
@@ -102,24 +144,16 @@ def pause_music():
 def rewind_music():
     play_music()
     current_file = strip_filename(filename)
-    statusbar['text'] = "Restarted Playback of" + ' ' + current_file
+    selected_song = lboxPlaylist.curselection()
+    selected_song = int(selected_song[0])
+    play_selection = playlist[selected_song]
+    nowplaying = strip_filename(play_selection)
+    statusbar['text'] = "Restarted Playback of" + ' ' + nowplaying
 
 def set_vol(val):
     volume = int(val) / 100
     mixer.music.set_volume(volume) # <-- This function takes float value from 0 to 1
     print("Volume set to: ", val)
-
-# Utility function for getting just the filename with no path or extension
-def strip_filename(filename):
-    tmp = os.path.basename(filename)
-    nowplaying = os.path.splitext(tmp)[0]
-    return nowplaying
-
-def resize_btn_image(path_to_img, x_dim, y_dim):
-    original_image = Image.open(path_to_img)
-    original_image = original_image.resize((x_dim, y_dim), Image.ANTIALIAS)
-    new_image = ImageTk.PhotoImage(original_image)
-    return new_image
 
 muted = False
 def mute_audio():
@@ -136,6 +170,11 @@ def mute_audio():
         scale.set(0)
         muted = True
 
+def on_close():
+    tkinter.messagebox.showinfo('Shutdown', 'Shutting down...')
+    stop_music()
+    root.destroy()
+
 #========
 # MAIN
 #========
@@ -143,12 +182,33 @@ def mute_audio():
 # Window layout and setup
 # Layout setup
 root = Tk()
-middle_frame = Frame(root)
-middle_frame.pack(padx=30, pady=30)
+
+# Status Bar
+statusbar = Label(root, text='Welcome to Diapason', anchor=W, bd=10, relief=FLAT)
+statusbar.pack(side=BOTTOM, fill=X)
 
 # Basic window
 root.title("Diapason")
 root.iconbitmap(r'assets/diapason.ico')
+
+# Left/Right Frames
+left_frame = Frame(root)
+left_frame.pack(side=LEFT, padx=30)
+
+right_frame = Frame(root)
+right_frame.pack()
+
+# Top Frame
+top_frame = Frame(right_frame)
+top_frame.pack(side=TOP)
+
+# Bottom Frame
+bottom_frame = Frame(right_frame)
+bottom_frame.pack(side=BOTTOM)
+
+# Middle Frame
+middle_frame = Frame(right_frame)
+middle_frame.pack(padx=30, pady=30)
 
 # Top Menubar
 menubar = Menu(root)
@@ -168,11 +228,11 @@ menubar.add_cascade(label="Help", menu=sub_menu)
 sub_menu.add_command(label="About", command=about_us)
 
 # Show media length
-lblLength = Label(root, text = "--:--")
+lblLength = Label(top_frame, text = "Total Length: --:--")
 lblLength.pack()
 
 # Show current play time
-lblCurrent = Label(root, text="--:--")
+lblCurrent = Label(top_frame, text="Current Time: --:--")
 lblCurrent.pack()
 
 # Initialize Pygame Mixer class for audio playback
@@ -181,9 +241,9 @@ mixer.init()
 global flat_red
 flat_red = '#C93C3E'
 
-# Main Screen label.
-#text = Label(middle_frame, relief=GROOVE, text = 'Make a joyful noise!')
-#text.grid(row=0, column=2)
+# Playlist Listbox
+lboxPlaylist = Listbox(left_frame)
+lboxPlaylist.pack()
 
 # Pause Button
 pauseImg = resize_btn_image('assets/pause.png', 50, 50)
@@ -195,13 +255,20 @@ playImg = PhotoImage(file='assets/play.png')
 btnPlay = Button(middle_frame, image = playImg, relief=FLAT, command=play_music)
 btnPlay.grid(row=1, column=1)
 
+# Add Song Button
+addImg = resize_btn_image('assets/add.png', 32, 32)
+btnAdd = Button(left_frame, image=addImg, relief=FLAT, command=browse_file)
+btnAdd.pack(side=LEFT)
+
+# Remove Song Button
+removeImg = resize_btn_image('assets/remove.png', 32, 32)
+btnRemove = Button(left_frame, image=removeImg, relief=FLAT, command=remove_item)
+btnRemove.pack(side=LEFT)
+
 # Stop Button
 stopImg = resize_btn_image('assets/stop.png', 50, 50)
 btnStop = Button(middle_frame, image=stopImg, relief=FLAT,command=stop_music)
 btnStop.grid(row=1, column=2)
-
-bottom_frame = Frame(root)
-bottom_frame.pack()
 
 # Rewind Button
 rewindImg = resize_btn_image('assets/rewind.png', 35, 35)
@@ -212,16 +279,15 @@ btnRewind.grid(row=0, column=0, pady=20)
 volumeImg = resize_btn_image('assets/volume.png', 35, 35)
 mutedImg = resize_btn_image('assets/muteVolume.png', 35, 35)
 btnMute = Button(bottom_frame, image=volumeImg, relief=FLAT, command=mute_audio)
-btnMute.grid(row=0, column=2)
+btnMute.grid(row=0, column=1)
 
 # Volume Scale
 scale = Scale(bottom_frame,from_=0, to=100, orient=HORIZONTAL, length=250, sliderlength=25, sliderrelief=FLAT, troughcolor=flat_red, command=set_vol)
 scale.set(70)
 mixer.music.set_volume(0.7) # Set default volume
-scale.grid(row=0, column=1, padx=30)
+scale.grid(row=0, column=2, padx=15)
 
-# Status Bar
-statusbar = Label(root, text='Welcome to Diapason', anchor=W, bd=10, relief=FLAT)
-statusbar.pack(side=BOTTOM, fill=X)
 
+
+root.protocol("WM_DELETE_WINDOW", on_close)
 root.mainloop()
